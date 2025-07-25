5bc8c5d1417585c761ffb8bee40fbd180c2c1716
David Stahl-Gruber <david@gruber.tv>
2025-07-25 19:22:28 +0200

    feat: added dark mode with toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from '@/views/App.tsx'
+import App from '@/components/App.tsx'
 import { RegistryContext } from '@effect-rx/rx-react'
 import { stateRegistry } from '@/state.ts'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RegistryContext.Provider value={stateRegistry}>
-      <App />
-    </RegistryContext.Provider>
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <RegistryContext.Provider value={stateRegistry}>
+        <App />
+      </RegistryContext.Provider>
+    </ThemeProvider>
   </StrictMode>,
 )
