d8e419967b8e97323cd6fa448d8bb0e8cd04552f
David Stahl-Gruber <david@gruber.tv>
2025-11-16 12:48:46 +0100

    chore: updated vitest config to not include docs/ repos

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
 		globals: true,
 		environment: "jsdom",
 		setupFiles: [],
+		exclude: [
+			"**/node_modules/**",
+			"**/dist/**",
+			"**/docs/**",
+			"**/.{idea,git,cache,output,temp}/**",
+			"**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
+		],
 	},
 	resolve: {
 		alias: {
