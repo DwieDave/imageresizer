ac0c9a3daabbf7949a7a177ecf94bcc163b784f9
David Stahl-Gruber <david@gruber.tv>
2025-11-28 17:48:04 +0100

    chore: remove pnpm and exclude docs/

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,22 +3,22 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: [],
-		exclude: [
-			"**/node_modules/**",
-			"**/dist/**",
-			"**/docs/**",
-			"**/.{idea,git,cache,output,temp}/**",
-			"**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
-		],
-	},
-	resolve: {
-		alias: {
-			"@": resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: [],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/docs/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
+    ],
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
