a31d68b8b8887df0062f570afc889ea1e34852c7
David Stahl-Gruber <david@gruber.tv>
2025-11-28 17:13:53 +0100

    fix: slider re-render issue after react compiler

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -47,10 +47,10 @@ function Slider({
 					)}
 				/>
 			</SliderPrimitive.Track>
-			{_values.map((val, index) => (
+			{_values.map((_, index) => (
 				<SliderPrimitive.Thumb
 					data-slot="slider-thumb"
-					key={`thumb-${index}-${val}`}
+					key={`thumb-${index}`}
 					className="border-primary bg-background ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
 				/>
 			))}
